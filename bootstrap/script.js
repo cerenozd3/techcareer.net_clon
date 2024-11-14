01954e8c6b6fb479a0ac05442dd0a6d513699122
cerenozd3 <169135581+cerenozd3@users.noreply.github.com>
2024-11-15 01:10:56 +0300

    demo 1

---
+++ bootstrap/script.js
@@ -0,0 +1,3 @@
+document.getElementById("seeAllComments").addEventListener("click", function() {
+    window.location.href = "user-stories.html"; // Kendi sayfanızın dosya yolunu buraya ekleyin
+});
